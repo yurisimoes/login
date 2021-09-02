3e77819cf926d3f5be6eaf6694e270fd3bcc16af
yuri <yurisimoes37@gmail.com>
2021-09-02 11:38:52 -0300

    feat: User and OpenIddict migrations

---
+++ Api/Migrations/20210902141958_v1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    username = table.Column<string>(type: "text", nullable: true),
+                    username_normalized = table.Column<string>(type: "text", nullable: true),
+                    hash = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "id", "hash", "username", "username_normalized" },
+                values: new object[] { new Guid("24e2a132-c2e1-4a11-97a1-4dfb2fc524a0"), null, "Admin", "admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ Api/Migrations/20210902142350_v2.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OpenIddictApplications",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    client_id = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    client_secret = table.Column<string>(type: "text", nullable: true),
+                    concurrency_token = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    consent_type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    display_name = table.Column<string>(type: "text", nullable: true),
+                    display_names = table.Column<string>(type: "text", nullable: true),
+                    permissions = table.Column<string>(type: "text", nullable: true),
+                    post_logout_redirect_uris = table.Column<string>(type: "text", nullable: true),
+                    properties = table.Column<string>(type: "text", nullable: true),
+                    redirect_uris = table.Column<string>(type: "text", nullable: true),
+                    requirements = table.Column<string>(type: "text", nullable: true),
+                    type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_open_iddict_applications", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OpenIddictScopes",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    concurrency_token = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    descriptions = table.Column<string>(type: "text", nullable: true),
+                    display_name = table.Column<string>(type: "text", nullable: true),
+                    display_names = table.Column<string>(type: "text", nullable: true),
+                    name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    properties = table.Column<string>(type: "text", nullable: true),
+                    resources = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_open_iddict_scopes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OpenIddictAuthorizations",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    application_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    concurrency_token = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    creation_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    properties = table.Column<string>(type: "text", nullable: true),
+                    scopes = table.Column<string>(type: "text", nullable: true),
+                    status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    subject = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: true),
+                    type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_open_iddict_authorizations", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_open_iddict_authorizations_open_iddict_applications_application",
+                        column: x => x.application_id,
+                        principalTable: "OpenIddictApplications",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OpenIddictTokens",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    application_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    authorization_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    concurrency_token = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    creation_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    expiration_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    payload = table.Column<string>(type: "text", nullable: true),
+                    properties = table.Column<string>(type: "text", nullable: true),
+                    redemption_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    reference_id = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    subject = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: true),
+                    type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_open_iddict_tokens", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_open_iddict_tokens_open_iddict_applications_application_id",
+                        column: x => x.application_id,
+                        principalTable: "OpenIddictApplications",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_open_iddict_tokens_open_iddict_authorizations_authorization_id",
+                        column: x => x.authorization_id,
+                        principalTable: "OpenIddictAuthorizations",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_open_iddict_applications_client_id",
+                table: "OpenIddictApplications",
+                column: "client_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_open_iddict_authorizations_application_id_status_subject_type",
+                table: "OpenIddictAuthorizations",
+                columns: new[] { "application_id", "status", "subject", "type" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_open_iddict_scopes_name",
+                table: "OpenIddictScopes",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_open_iddict_tokens_application_id_status_subject_type",
+                table: "OpenIddictTokens",
+                columns: new[] { "application_id", "status", "subject", "type" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_open_iddict_tokens_authorization_id",
+                table: "OpenIddictTokens",
+                column: "authorization_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_open_iddict_tokens_reference_id",
+                table: "OpenIddictTokens",
+                column: "reference_id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OpenIddictScopes");
+
+            migrationBuilder.DropTable(
+                name: "OpenIddictTokens");
+
+            migrationBuilder.DropTable(
+                name: "OpenIddictAuthorizations");
+
+            migrationBuilder.DropTable(
+                name: "OpenIddictApplications");
+        }
+    }
+}
